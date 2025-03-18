397ca4dfd98f7735665e04a36f2db4cbc992e5c5
Ethan22112 <22112@fraser.school.nz>
2025-03-19 11:22:42 +1300

    String Checker
    Making a program that checks that users enter the full word or the first letter of a word from a list of valid responses, work in progress as outcome does not match expected outcome

---
+++ C_05_String_Checker.py
@@ -0,0 +1,27 @@
+# Functions Go Here
+def string_check(question, valid_ans_list):
+    """Checks that users enter the full word
+       or the first letter of a word from a list of valid responses"""
+    
+    while True:
+
+        response = input(question).lower()
+
+        for item in valid_ans_list:
+            # checks if the response is either exactly the same as the word
+            if response == item:
+                return item
+
+            # checks if the response is exactly the first letter of the word
+            elif response == item[0]:
+                return item
+            
+            print(f'Please choose an option from {valid_ans_list}')
+    
+# Main Routine Goes Here
+levels = ['easy', 'medium', 'hard']
+
+like_coffee = string_check('Do You Like Coffee? ', valid_ans_list = ['yes', 'no'])
+print(f'You Chose {like_coffee}')
+Difficulty = string_check('Set Your Difficulty ', levels)
+print(f'You Chose {Difficulty}')
