29f3182400fad28886852646f3e8d39bb942f9d0
Ethan22112 <22112@fraser.school.nz>
2025-03-25 10:01:24 +1300

    Integer Checker V1
    an integer checker that checks whether the users have entered an integer.

---
+++ C_05_integer_checker_v1.py
@@ -0,0 +1,33 @@
+def num_check(question):
+    """Checks if user(s) enter an integer / float that is more than zero
+       (or optional exit code)"""
+    
+    error = "Oops, please enter an integer that is more than zero"
+    
+    while True:
+        response = input(question).lower
+
+        #checks for the exit code
+        if response == "xxx":
+            return response
+        
+        try:
+            #changes the response into an integer, checking whether its value is more than zero
+            response = int(response)
+
+            if response > 0:
+               return response
+            else:
+                print(error) 
+
+        except ValueError: 
+            print(error)
+
+
+#main routine goes here
+
+#loop for testing purposes
+while True:
+    print()
+
+
